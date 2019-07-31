00f1386bb780e424c8bfe82a3f1bd368ef14c1de
Jessie <jessie.jiang22@gmail.com>
2019-07-31 15:07:09 -0700

    delete

--- getQuotes.js
+++
@@ -1,6 +0,0 @@
-unirest.get("https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium")
-.header("X-RapidAPI-Host", "healthruwords.p.rapidapi.com")
-.header("X-RapidAPI-Key", "SIGN-UP-FOR-KEY")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
